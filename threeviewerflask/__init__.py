2666ef6016feb85954186d5656f90ce4910864c7
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-08-03 19:28:40 +0200

    update

--- threeviewerflask/__init__.py
+++ threeviewerflask/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, url_for, redirect, request
+from flask import render_template
 
 
 # Create flask app instance
@@ -10,4 +10,9 @@ app.config['SECRET_KEY'] = 'afs87fas7bfsa98fbasbas98fh78oizu'
 
 @app.route('/')
 def home():
-    return render_template('viewer.html')
+    return render_template('index.html')
+
+
+@app.route('/f')
+def f():
+    return render_template('f.html')
