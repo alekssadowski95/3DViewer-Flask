7dd5ea90b0da2d94e77f0584b75b5141228cbee6
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-08-03 19:44:13 +0200

    update

--- threeviewerflask/__init__.py
+++ threeviewerflask/__init__.py
@@ -8,11 +8,6 @@ application = app = Flask(__name__)
 # Add secret key
 app.config['SECRET_KEY'] = 'afs87fas7bfsa98fbasbas98fh78oizu'
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/f')
-def f():
-    return render_template('f.html')
+@app.route('/viewer/<filename>')
+def viewer(filename):
+    return render_template('viewer.html', filename = filename)
